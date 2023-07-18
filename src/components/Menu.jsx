97e9cc9964d77d881ab8f36cfd1273448b2aa6f5
Zasada94 <pawelzasada2@wp.pl>
2023-07-18 09:50:51 +0200

    rwd and structure changes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,15 @@
 import { styled } from "styled-components";
 import LocalPizzaIcon from "@mui/icons-material/LocalPizza";
 import { full } from "../responsive";
+import Product from "./Product";
 
 const Container = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
+	max-width: 1200px;
+	margin: 0 auto;
 `;
 
 const Title = styled.h1`
@@ -28,76 +31,6 @@ const Subtitle = styled.h2`
 const Wrapper = styled.div`
 	display: flex;
 	flex-wrap: wrap;
-	${full({
-		flexWrap: "nowrap",
-	})}
-`;
-
-const ItemWrapper = styled.div`
-	display: flex;
-	justify-content: space-around;
-	border: 0.5px solid none;
-	border-radius: 15px;
-	box-shadow: 0 10px 45px 10px #26282b0f;
-	margin: 15px 40px;
-	padding: 15px 15px;
-	flex-grow: 1;
-	${full({
-		margin: "15px 20px",
-	})}
-`;
-
-const ItemImage = styled.img`
-	width: 150px;
-	margin-right: 20px;
-`;
-
-const RightWrapper = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	flex-grow: 1;
-`;
-
-const ItemTitle = styled.h2`
-	font-size: 20px;
-	font-weight: 400;
-	flex: 1;
-`;
-
-const ItemDesc = styled.p`
-	font-size: 14px;
-	font-weight: 300;
-	flex: 1;
-`;
-
-const OthersWrapper = styled.div`
-	display: flex;
-	justify-content: space-between;
-	flex: 3;
-	align-items: end;
-`;
-
-const Price = styled.div`
-	padding: 5px 10px 5px;
-`;
-
-const AddButton = styled.button`
-	color: white;
-	border: 1px solid rgba(0, 0, 0, 0);
-	border-radius: 10px;
-	padding: 5px 10px;
-	margin-right: 10px;
-	word-spacing: 15px;
-	background-color: #be3144;
-	cursor: pointer;
-	font-weight: 500;
-	transition: 0.5s ease-out;
-	&:hover {
-		background-color: white;
-		border: 1px solid #be3144;
-		color: black;
-	}
 `;
 
 const Menu = () => {
@@ -109,28 +42,10 @@ const Menu = () => {
 				Pizza
 			</Subtitle>
 			<Wrapper>
-				<ItemWrapper>
-					<ItemImage src="https://i.ibb.co/bNVvQnK/Screenshot-1.png"></ItemImage>
-					<RightWrapper>
-						<ItemTitle>DOUBLE PEPERONI</ItemTitle>
-						<ItemDesc>Double: tomato sauce, mozarella, pepperoni</ItemDesc>
-						<OthersWrapper>
-							<Price>30 PLN</Price>
-							<AddButton>Dodaj +</AddButton>
-						</OthersWrapper>
-					</RightWrapper>
-				</ItemWrapper>
-				<ItemWrapper>
-					<ItemImage src="https://i.ibb.co/bNVvQnK/Screenshot-1.png"></ItemImage>
-					<RightWrapper>
-						<ItemTitle>DOUBLE PEPERONI</ItemTitle>
-						<ItemDesc>Double: tomato sauce, mozarella, pepperoni</ItemDesc>
-						<OthersWrapper>
-							<Price>30 PLN</Price>
-							<AddButton>Dodaj +</AddButton>
-						</OthersWrapper>
-					</RightWrapper>
-				</ItemWrapper>
+				<Product />
+				<Product />
+				<Product />
+				<Product />
 			</Wrapper>
 		</Container>
 	);
---
+++ src/components/Product.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import { styled } from "styled-components";
+import { full, mobile } from "../responsive";
+
+const ItemWrapper = styled.div`
+	display: flex;
+	justify-content: space-around;
+	border: 0.5px solid none;
+	border-radius: 15px;
+	box-shadow: 0 10px 45px 10px #26282b0f;
+	margin: 15px 40px;
+	padding: 15px 15px;
+	flex-grow: 1;
+	flex-basis: 100%;
+	${full({
+		margin: "15px 20px",
+		flexBasis: "40%",
+	})}
+	${mobile({
+		margin: "10px 20px",
+		padding: "10px 10px",
+	})}
+`;
+
+const ItemImage = styled.img`
+	width: 150px;
+	height: 150px;
+	margin-right: 20px;
+	align-self: center;
+	${mobile({
+		width: "130px",
+		height: "130px",
+	})}
+`;
+
+const RightWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	flex-grow: 1;
+`;
+
+const ItemTitle = styled.h2`
+	font-size: 20px;
+	font-weight: 400;
+	flex: 1;
+	${mobile({
+		fontSize: "18px",
+	})}
+`;
+
+const ItemDesc = styled.p`
+	font-size: 14px;
+	font-weight: 300;
+	flex: 1;
+	${mobile({
+		fontSize: "12px",
+	})}
+`;
+
+const OthersWrapper = styled.div`
+	display: flex;
+	justify-content: space-between;
+	flex: 3;
+	align-items: end;
+`;
+
+const Price = styled.div`
+	padding: 5px 10px 5px;
+	${mobile({
+		fontSize: "14px",
+		padding: " 2px 5px 2px",
+	})}
+`;
+
+const AddButton = styled.button`
+	color: white;
+	border: 1px solid rgba(0, 0, 0, 0);
+	border-radius: 10px;
+	padding: 5px 10px;
+	margin-right: 10px;
+	word-spacing: 10px;
+	background-color: #be3144;
+	cursor: pointer;
+	font-weight: 500;
+	transition: 0.5s ease-out;
+	&:hover {
+		background-color: white;
+		border: 1px solid #be3144;
+		color: black;
+	}
+	${mobile({
+		fontSize: "12px",
+		wordSpacing: "3px",
+		marginRight: "5px",
+	})}
+`;
+
+const Product = () => {
+	return (
+		<ItemWrapper>
+			<ItemImage src="https://i.ibb.co/bNVvQnK/Screenshot-1.png"></ItemImage>
+			<RightWrapper>
+				<ItemTitle>DOUBLE PEPERONI</ItemTitle>
+				<ItemDesc>Double: tomato sauce, mozarella, pepperoni</ItemDesc>
+				<OthersWrapper>
+					<Price>30 PLN</Price>
+					<AddButton>Dodaj +</AddButton>
+				</OthersWrapper>
+			</RightWrapper>
+		</ItemWrapper>
+	);
+};
+
+export default Product;
