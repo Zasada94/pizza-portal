80ca70cbfe04b902c6d60f4a2ab9711f3a60585f
Zasada94 <pawelzasada2@wp.pl>
2023-07-19 13:57:33 +0200

    product page finished

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,68 +2,95 @@ import { Add, Remove } from "@mui/icons-material";
 import { useState } from "react";
 import { styled } from "styled-components";
 import Navbar from "../components/Navbar";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
-	padding: 50px;
+	padding: 30px;
+	margin: 30px auto;
+	max-width: 500px;
 	display: flex;
+	flex-direction: column;
+	align-items: center;
+	border: 0.5px solid none;
+	border-radius: 15px;
+	box-shadow: 0 10px 45px 10px #26282b0f;
+	${mobile({
+		maxWidth: "80vw",
+		padding: "20px 10px",
+	})}
 `;
 
 const ImgContainer = styled.div`
-	flex: 1;
+	width: 200px;
+	overflow: hidden;
 `;
 
 const Image = styled.img`
-	width: 100%;
-	height: 90vh;
-	object-fit: contain;
+	object-fit: cover;
+	margin: 0 auto;
+	width: 200px;
+	height: 200px;
 `;
 
 const InfoContainer = styled.div`
-	flex: 1;
 	padding: 0px 50px;
+	${mobile({
+		padding: "0 10px",
+	})}
 `;
 
 const Title = styled.h1`
-	font-weight: 200;
+	font-size: 20px;
+	font-weight: 400;
+	flex: 1;
+	${mobile({
+		fontSize: "18px",
+	})}
 `;
 
 const Desc = styled.p`
-	margin: 20px 0px;
+	margin: 10px 0px;
+	font-size: 14px;
+	font-weight: 300;
+	flex: 1;
+	${mobile({
+		fontSize: "12px",
+	})}
 `;
 
 const Price = styled.span`
-	font-weight: 100;
-	font-size: 40px;
+	${mobile({
+		fontSize: "14px",
+	})}
 `;
 
-const FilterContainer = styled.div`
-	width: 50%;
+const Filter = styled.div`
+	width: 100%;
 	margin: 30px 0;
 	display: flex;
 	justify-content: space-between;
 `;
 
-const Filter = styled.div`
-	display: flex;
-	align-items: center;
-`;
-
 const FilterTitle = styled.span`
-	font-size: 20px;
-	font-weight: 200;
+	align-self: center;
+	font-weight: 300;
+	font-size: 16px;
+	${mobile({
+		fontSize: "14px",
+		padding: " 2px 5px 2px",
+	})}
 `;
 
 const FilterSize = styled.select`
-	margin-left: 10px;
 	padding: 5px;
 `;
 
 const FilterSizeOption = styled.option``;
 
 const AddContainer = styled.div`
-	width: 50%;
+	width: 100%;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
@@ -79,7 +106,7 @@ const Amount = styled.span`
 	width: 30px;
 	height: 30px;
 	border-radius: 10px;
-	border: 1px solid teal;
+	border: 1px solid var(--red);
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -87,14 +114,22 @@ const Amount = styled.span`
 `;
 
 const Button = styled.button`
-	padding: 15px;
-	border: 2px solid teal;
-	background-color: white;
+	color: white;
+	border: 1px solid rgba(0, 0, 0, 0);
+	border-radius: 10px;
+	padding: 5px 10px;
+	background-color: #be3144;
 	cursor: pointer;
 	font-weight: 500;
+	transition: 0.5s ease-out;
 	&:hover {
-		background-color: #f8f4f4;
+		background-color: white;
+		border: 1px solid #be3144;
+		color: black;
 	}
+	${mobile({
+		fontSize: "12px",
+	})}
 `;
 
 const Product = () => {
@@ -120,18 +155,16 @@ const Product = () => {
 				<InfoContainer>
 					<Title>Double Pepperoni</Title>
 					<Desc>Double: tomato sauce, mozarella, pepperoni</Desc>
-					<Price>30 PLN</Price>
-					<FilterContainer>
-						<Filter>
-							<FilterTitle>Size</FilterTitle>
-							<FilterSize>
-								<FilterSizeOption>SMALL 30cm</FilterSizeOption>
-								<FilterSizeOption>MEDIUM 40cm</FilterSizeOption>
-								<FilterSizeOption>LARGE 50cm</FilterSizeOption>
-							</FilterSize>
-						</Filter>
-					</FilterContainer>
+					<Filter>
+						<FilterTitle>Size</FilterTitle>
+						<FilterSize>
+							<FilterSizeOption>SMALL 30cm</FilterSizeOption>
+							<FilterSizeOption>MEDIUM 40cm</FilterSizeOption>
+							<FilterSizeOption>LARGE 50cm</FilterSizeOption>
+						</FilterSize>
+					</Filter>
 					<AddContainer>
+						<Price>30 PLN</Price>
 						<AmountContainer>
 							<Remove onClick={() => handleQuantity("dec")} />
 							<Amount>{quantity}</Amount>
